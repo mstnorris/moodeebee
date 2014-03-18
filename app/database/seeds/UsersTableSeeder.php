37ac91182a26471e92e647163283a03678b225d2
Michael Norris <github@michaelnorris.co.uk>
2014-03-18 21:46:15 +0000

    Edited to seed two users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,8 +12,17 @@ class UsersTableSeeder extends Seeder {
 		foreach(range(1, 10) as $index)
 		{
 			User::create([
-
+                'name' => 'Mike',
+                'username' => 'mstnorris',
+                'email' => 'mstnorris@gmail.com',
+                'password' => 'password'
 			]);
+            User::create([
+                'name' => 'Holly',
+                'username' => 'hjmcnicol',
+                'email' => 'holly.mcnicol@live.co.uk',
+                'password' => 'password'
+            ]);
 		}
 	}
 
